34f445130179bdaf2c2728193e13c075f1b72fbc
Toasti6 <lukenole6@gmail.com>
2021-08-25 18:58:07 -0400

    small program 0.1

--- smallprogram0_TempletonLuke.c
+++ smallprogram0_TempletonLuke.c
@@ -3,7 +3,7 @@
 
 int main() //main function
 {
-	printf("My name is Luke Templeton\n"); //display statement to the screen, followed by 3 more lines
+	printf("\nMy name is Luke Templeton\n"); //display statement to the screen, followed by 3 more lines
 	printf("\nI am a student at the University of Central Florida\n");
 	printf("\nThis Fall 2021, I am taking the COP3223C Introduction to Programming with C with Dr. Steinberg.\n");
 	printf("\nI am very excited to learn the C Language this semester!\n");
