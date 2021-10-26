d500391221805fe9e01d6484b63e0fbc8d7c2868
Toasti6 <lukenole6@gmail.com>
2021-10-26 14:32:03 -0400

    sp6 0.1

---
+++ smallprogram6_TempletonLuke.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void meanMinMax(int meanArr[]);
+void organize(int meanArr[]);
+void duplicate(int meanArr[], int newArr[]);
+void locate();
+
+int main(void)
+{
+	
+
+	return 0;
+}
